f093ff45176d0f3bb2de9a298d03107ae45e16ab
Brittany French <bb@sagareus.com>
2026-06-22 16:53:54 -0500

    Remove 'Typically' from covered/not-covered column headers

--- src/app/warranties/page.tsx
+++ src/app/warranties/page.tsx
@@ -246,7 +246,7 @@ export default function WarrantiesPage() {
                     d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                   />
                 </svg>
-                Typically Covered
+                Covered
               </h3>
               <ul className="space-y-3">
                 {[
@@ -291,7 +291,7 @@ export default function WarrantiesPage() {
                     d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
                   />
                 </svg>
-                Not Typically Covered
+                Not Covered
               </h3>
               <ul className="space-y-3">
                 {[
